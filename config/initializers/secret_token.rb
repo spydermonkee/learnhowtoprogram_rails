db6bd0ae93bdf9ca8a622f58757882012ee8c0c5
Kimberlee Redman & Spyder Monkee McHenry <student@epicodus.com>
2014-04-02 09:11:47 -0700

    intial commit, structure setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodeSite::Application.config.secret_key_base = '9c2d33c8d692b86c82fac645dde5bcd4b0e59bf848e481f992c6953f03006766f9d8be3d8a121eaef0d447189dd74f899d3209e691d56409465bfc267dbbfdbf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodeSite::Application.config.session_store :cookie_store, key: '_code_site_session'
