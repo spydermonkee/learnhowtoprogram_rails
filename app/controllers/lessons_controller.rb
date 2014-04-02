789f64585894f771952fb5faa598e4e65ccee645
Kimberlee Redman & Spyder Monkee McHenry <student@epicodus.com>
2014-04-02 11:30:45 -0700

    improves all functionality and connections of lesson mvc

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,4 +8,18 @@ class LessonsController < ApplicationController
     @lesson = Lesson.find(params[:id])
     render('lessons/show.html.erb')
   end
+
+  def create
+    @lesson = Lesson.new(params[:lesson])
+    if @lesson.save
+      redirect_to('/lessons')
+    else
+      render('lessons/new')
+    end
+  end
+
+  def new
+    @lesson = Lesson.new
+    render('lessons/new.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 CodeSite::Application.routes.draw do
   match('lessons', { :via => :get, :to => 'lessons#index'})
+  match('lessons/new', { :via => :get, :to => 'lessons#new'})
   match('lessons/:id', { :via => :get, :to => 'lessons#show'})
+  match('lessons/new', { :via => :post, :to => 'lessons#create'})
+  # match('lessons/edit', )
+
 end
